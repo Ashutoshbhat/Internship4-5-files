b3c57909040c6d30fe561e2dea3fe360dcb69e5c
Ashutosh bhat <167815630+Ashutoshbhat@users.noreply.github.com>
2025-01-19 16:33:44 +0530

    Add files via upload
    files i worked on during my internship at winnovation.

---
+++ first_program.py
@@ -0,0 +1,55 @@
+'''print("welcome to day 1 of machine learning!")
+print("now this is the second line of the program that will be executed")
+
+num1 =  100 
+name = 'sameer'
+
+if num1>90:
+	print("number is greater than 90")
+elif num1>100:
+	print("the number is greater than 100")
+
+else:
+	print("idk what the number is")
+
+num2 = num1 *12
+print("the number after multiplication is", num2)
+
+print("the value stored in string variable is", name)
+print("the first three charcters of name variable are", name[0:3])
+
+bool_var = False
+
+if bool_var:
+	print("inside if block")'''
+
+some_string = "some value of type string "
+print("orginal string is ", some_string)
+
+some_string = some_string.replace(' ','_')
+print("modified string is ", some_string)
+
+print(type(some_string))
+
+num1 = 100
+
+print("the type of num1 is ", type(num1))
+
+print("the length of some_string is ", len(some_string))
+
+print(f"the string some_string contains {some_string}")
+
+print(f"the num1 variable contains {num1} and num1 after addition of 100 is {num1+100}")
+
+str = "some other string"
+
+print("the last two characters of the string are", str[len(str)-3:len(str)+1])
+
+
+
+
+
+
+
+
+	
